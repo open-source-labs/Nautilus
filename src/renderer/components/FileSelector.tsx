8e3cf817909aa657f95878afa0cab6874d152581
Jordan <jlong009@outlook.com>
2022-06-24 19:11:24 -0400

    so close but yet, so far?

--- src/renderer/components/FileSelector.tsx
+++ src/renderer/components/FileSelector.tsx
@@ -66,7 +66,7 @@ const FileSelector: React.FC = () => {
         name="yaml"
         accept=".yml,.yaml"
         style={{ display: 'none' }}
-        onChange={async (event: React.SyntheticEvent<HTMLInputElement>) => {
+        onChange={(event: React.SyntheticEvent<HTMLInputElement>) => {
           // make sure there was something selected
           // console.log('FileSelector Event and event.currentTarget', event, event.currentTarget)
           if (event.currentTarget) {
@@ -81,20 +81,23 @@ const FileSelector: React.FC = () => {
                */
 
               // const fetchInfoFromFile = createAsyncThunk(
-              //   'File',
-              //   async (file: File, thunkAPI)
-              // );
+              //   'event/',
+              //   async (targetFile: File) => {
+              //     const openedFile = await fileOpen(targetFile); 
+              //     return await Array.isArray(openedFile) ? dispatch(fileOpenError(openedFile)) : dispatch(yamlToState(openedFile.yamlState)), dispatch(switchTab({filePath: openedFile.filePath, openFiles: openedFile.openFiles}));
+              //   });
 
               console.log(fileOpen(event.currentTarget.files[0]));
-              fileOpen(event.currentTarget.files[0]).then( (openedFile:any) =>{
+              // fetchInfoFromFile(event.currentTarget.files[0]);
+              
+
+
+              Promise.resolve(fileOpen(event.currentTarget.files[0])).then( (openedFile:any) =>{
                 console.log("THis is data: " ,openedFile);
                 Array.isArray(openedFile) ? dispatch(fileOpenError(openedFile)) : dispatch(yamlToState(openedFile.yamlState)), dispatch(switchTab({filePath: openedFile.filePath, openFiles: openedFile.openFiles}));
               });
-
-
-              // const openedFile = await fileOpen(event.currentTarget.files[0]);
+              // const openedFile = 
               // console.log('after file open, we get this far', openedFile);
-              // Array.isArray(openedFile) ? dispatch(fileOpenError(openedFile)) : dispatch(yamlToState(openedFile.yamlState)), dispatch(switchTab({filePath: openedFile.filePath, openFiles: openedFile.openFiles}));
             }
           }
         }}
